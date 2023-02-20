eff831e9b126b4af5b1ed33e360d854b89684937
Thejashree <thejashreehk@gmail.com>
2023-02-20 15:11:36 +0530

    refactor: move FizzBuzzService object creation to setUp method

--- src/test/java/com/spring/fizzBuzzV2/service/FizzBuzzServiceTest.java
+++ src/test/java/com/spring/fizzBuzzV2/service/FizzBuzzServiceTest.java
@@ -1,21 +1,27 @@
 package com.spring.fizzBuzzV2.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzServiceTest {
 
+    private FizzBuzzService service;
+
+    @BeforeEach
+    public void setup() {
+        service = new FizzBuzzService();
+    }
+
     @Test
     public void testNumber() {
-        FizzBuzzService service = new FizzBuzzService();
         String actualResult = service.getSubstitution(1);
         assertEquals("1", actualResult);
     }
 
     @Test
     public void testFizzDivisibleBy3(){
-        FizzBuzzService service = new FizzBuzzService();
         String actualResult = service.getSubstitution(9);
         assertEquals("fizz", actualResult);
     }
